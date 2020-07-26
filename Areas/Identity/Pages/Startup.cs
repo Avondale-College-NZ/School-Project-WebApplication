a21633caf108a5e99bedce40fb0f03b08b61c051
Benny Lu <ac98777@avcol.school.nz>
2020-07-27 10:54:53 +1200

    Added more policy privacy information

--- Areas/Identity/Pages/Startup.cs
+++ Areas/Identity/Pages/Startup.cs
@@ -40,8 +40,10 @@ namespace School_Project_WebApplication
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+           
 
             app.UseRouting();
 

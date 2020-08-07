bce09719c566f5c1f7c3852cae4a07cc867734ee
Benny Lu <ac98777@avcol.school.nz>
2020-08-07 12:29:46 +1200

    Adding a new role

---
+++ Controllers/AdmininstrationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School_Project_WebApplication.Controllers
+{
+    public class AdmininstrationController : Controller
+    {
+        public class AdministrationContoller (RoleManager<IdentityRole> roleManager)
+            {
+
+
+            }
+}
+
+            

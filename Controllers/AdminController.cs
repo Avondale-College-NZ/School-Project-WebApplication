33f2d5aa7a4bb8cc793e311fcee7370ad311eab2
Benny Lu <ac98777@avcol.school.nz>
2020-09-07 10:59:59 +1200

    Added a class role into the models folder and added a admin controller to the controller folder

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using School_Project_WebApplication.Models;
+
+namespace School_Project_WebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManger;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManger = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Create(Role role)
+        {
+            var roleExist = await roleManger.RoleExistsAsync(role.RoleName);
+            if(!roleExist)
+            {
+                var result = await roleManger.CreateAsync(new IdentityRole(role.RoleName));
+            }
+            return View();
+        }
+    }
+}

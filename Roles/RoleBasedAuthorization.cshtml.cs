45097afc0a2fd86e38a2291b943e19b98ffc52ca
Benny Lu <ac98777@avcol.school.nz>
2020-09-10 13:59:46 +1200

    Trying to add roles

---
+++ Roles/RoleBasedAuthorization.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace School_Project_WebApplication.Roles
+{
+    public class RoleBasedAuthorizationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

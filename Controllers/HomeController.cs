f28d1cee4d19f36d1504ad3f0219b4cfdf1e4910
Benny Lu <ac98777@avcol.school.nz>
2020-09-22 10:14:31 +1200

    Editing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,12 +30,6 @@ namespace School_Project_WebApplication.Controllers
             return View();
         }
 
-        public IActionResult Aboutus()
-        {
-            return View();
-        }
-
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

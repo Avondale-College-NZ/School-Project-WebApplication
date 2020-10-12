7073aea873a9094f0be059659fa056a9cdbe3b44
Benny Lu <ac98777@avcol.school.nz>
2020-10-13 10:14:37 +1300

    Added more comments on the razor pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace School_Project_WebApplication.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
